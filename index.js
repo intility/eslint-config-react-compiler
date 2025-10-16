4e49cf3c8b79cd42e8524fe60d963eb9eac4a473
Herman Jensen <herman.jensen@intility.no>
2025-10-16 15:48:20 +0200

    build: ditch typescript

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
+// @ts-check
+
 export default defineConfig([
 	globalIgnores(["**/dist/", "**/build/"]),
+	tseslint.configs.base,
+	reactHooks.configs.flat.recommended,
 	{
-		extends: [tseslint.configs.base, reactHooks.configs.flat.recommended],
 		files: ["src/**/*.{js,jsx,ts,tsx}"],
 	},
 ]);

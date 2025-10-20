560b9dfae8a04b3b6b83b73509f98e6b13ce4681
Herman Jensen <herman.jensen@intility.no>
2025-10-20 10:57:16 +0200

    build: add types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,10 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
-// @ts-check
-
 export default defineConfig([
 	globalIgnores(["**/dist/", "**/build/"]),
 	tseslint.configs.base,
+	// @ts-ignore react doesn't export their types correctly
 	reactHooks.configs.flat.recommended,
 	{
 		files: ["src/**/*.{js,jsx,ts,tsx}"],
